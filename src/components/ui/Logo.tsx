044d1cf745555a7ef1f61d2e5533991eef5469da
shebll <shebl4900@gmail.com>
2026-07-13 17:27:21 +0300

    chore:

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import icon from "@/assets/icons/icon.svg";
 
 function Logo() {
   return (
     <div className="gap-2 flex items-center">
-      <Image src={icon} alt="Icon" width={18} height={20} />
+      <Image src={"/assets/icons/icon.svg"} alt="Icon" width={18} height={20} />
       <h1 className="text-lg font-bold leading-7 tracking-tight ">TASKLY</h1>
     </div>
   );
