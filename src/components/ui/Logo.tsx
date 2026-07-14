e2f8c974e35a79bd4dd4e428068b4775d7880a4f
shebll <shebl4900@gmail.com>
2026-07-14 19:12:29 +0300

    fix: Make logo navigate to "/" home

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Logo() {
   return (
-    <div className="gap-2 flex items-center">
+    <Link href="/" className="gap-2 flex items-center">
       <Image src={"/assets/icons/icon.svg"} alt="Icon" width={18} height={20} />
       <h1 className="text-lg font-bold leading-7 tracking-tight ">TASKLY</h1>
-    </div>
+    </Link>
   );
 }
 
