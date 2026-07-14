fd5b9ab857cd293a3812ebbc5d35204a420b9008
shebll <shebl4900@gmail.com>
2026-07-14 20:47:02 +0300

    feat: Create FormField and form sing up form component

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import FormComponent from "@/components/forms/SignUpFormComponent";
+import SectionTitle from "@/components/shared-ui/SectionTitle";
 
-function page() {
-  return <div>sign up</div>;
+function SignUp() {
+  return (
+    <div className="bg-card-background p-12 rounded-md flex flex-col  items-center">
+      {/* section title component */}
+      <SectionTitle
+        title="Create your workspace"
+        des="Join the editorial approach to task management."
+      />
+
+      {/* form component */}
+      <FormComponent />
+    </div>
+  );
 }
 
-export default page;
+export default SignUp;
