97bec6022073424b262546a785e367c82f97ba2e
shebll <shebl4900@gmail.com>
2026-07-15 13:36:33 +0300

    feet: Create RequirementItem reusable component

---
+++ src/features/auth/components/PasswordRequirements.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import RequirementItem from "./ui/RequirementItem";
+
+type Props = { password: string };
+
+function PasswordRequirements({ password }: Props) {
+  return (
+    <div className="w-full rounded-md bg-blue-100 p-[16px] flex flex-col gap-[8px]">
+      <RequirementItem
+        text="At least 8 characters"
+        completed={(password && password.length >= 8) || false}
+      />
+
+      <RequirementItem
+        text="One uppercase, lowercase, and digit"
+        completed={/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/.test(password) || false}
+      />
+
+      <RequirementItem
+        text="One special character"
+        completed={/[!@#$%^&*(),.?":{}|<>]/.test(password) || false}
+      />
+    </div>
+  );
+}
+
+export default PasswordRequirements;
