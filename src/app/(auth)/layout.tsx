4d5869924b6caddaaaa643946a301d5f15991b56
shebll <shebl4900@gmail.com>
2026-07-17 17:50:47 +0300

    ui: fix auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-7xl m-auto pr-6 md:pr-10 pl-6 md:pl-10">
+    <div className="max-w-7xl h-full m-auto pr-6 md:pr-10 pl-6 md:pl-10">
       <AuthHeader />
       <div className="pt-4 pb-12 flex justify-center">{children}</div>
     </div>
