3e23f27f140acabdc55e90b3bb02c053a9eceef7
shebll <shebl4900@gmail.com>
2026-07-20 14:55:45 +0300

    chore: update metadata add cover image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,35 @@ export const metadata: Metadata = {
     icon: "/assets/icons/icon.svg",
     apple: "/assets/icons/icon.svg",
   },
+  openGraph: {
+    title: "Taskly",
+    description:
+      "Taskly is a modern task and project management platform designed to help teams and individuals organize tasks, track progress, and boost productivity.",
+    url: "https://taskly-gules.vercel.app/",
+    siteName: "Taskly",
+    images: [
+      {
+        url: "/assets/imgs/Cover.png", // public/og-image.png
+        width: 1200,
+        height: 630,
+        alt: "Taskly - Task & Project Management Platform",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Taskly",
+    description:
+      "Taskly is a modern task and project management platform designed to help teams and individuals organize tasks, track progress, and boost productivity.",
+    images: ["/assets/imgs/Cover.png"],
+  },
+
+  alternates: {
+    canonical: "https://taskly-gules.vercel.app/",
+  },
 };
 
 export default function RootLayout({
