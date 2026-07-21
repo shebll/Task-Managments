1f62c76a22f2bb34d8c25d19c44258c0d005d640
shebll <shebl4900@gmail.com>
2026-07-21 20:24:41 +0300

    fix: fix add project return void

--- src/features/projects/api/projects-api.ts
+++ src/features/projects/api/projects-api.ts
@@ -2,7 +2,7 @@ import { apiClient } from "@/lib/api/api-client";
 import { AddProjectType } from "../types/types";
 
 export const addProject = (addProjectData: AddProjectType) => {
-  return apiClient<AddProjectType>("/rest/v1/projects", {
+  return apiClient<void>("/rest/v1/projects", {
     method: "POST",
     body: JSON.stringify(addProjectData),
   });
