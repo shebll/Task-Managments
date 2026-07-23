6ce683b3e2f5fdfb016ac756c3603b6866a6b348
shebll <shebl4900@gmail.com>
2026-07-23 14:50:15 +0300

    feat: update dashboardLinks const

---
+++ src/const/navigation.ts
@@ -0,0 +1,29 @@
+// lib/navigation.ts
+
+export const dashboardLinks = [
+  {
+    title: "Projects",
+    projectRoute: "/project",
+    desktopIcon: "/assets/icons/project-icon.svg",
+  },
+  {
+    title: "Project Epics",
+    projectRoute: "/epics",
+    desktopIcon: "/assets/icons/project-epics-icon.svg",
+  },
+  {
+    title: "Project Tasks",
+    projectRoute: "/tasks",
+    desktopIcon: "/assets/icons/project-tasks-icon.svg",
+  },
+  {
+    title: "Project Members",
+    projectRoute: "/members",
+    desktopIcon: "/assets/icons/project-members-icon.svg",
+  },
+  {
+    title: "Project Details",
+    projectRoute: "/edit",
+    desktopIcon: "/assets/icons/project-details-icon.svg",
+  },
+];
