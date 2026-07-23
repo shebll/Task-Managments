d204c73f9987705896f981e776735a2fdeea196e
shebll <shebl4900@gmail.com>
2026-07-23 13:49:34 +0300

    feat: project data type

--- src/features/projects/types/types.ts
+++ src/features/projects/types/types.ts
@@ -2,3 +2,11 @@ import z from "zod";
 import { addProjectSchema } from "../schema/add-project-schema";
 
 export type AddProjectType = z.infer<typeof addProjectSchema>;
+
+export type ProjectsData = {
+  id: string;
+  name: string;
+  description: string;
+  created_by: string;
+  created_at: string;
+}[];
