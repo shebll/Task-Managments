09cfca62df17d60901839c3ea221a36b01547a44
shebll <shebl4900@gmail.com>
2026-07-24 19:09:35 +0300

    feat: add project and update project page

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
       <div className="flex flex-1 flex-col">
         <DashboardHeader />
 
-        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto p-10">{children}</main>
       </div>
     </div>
   );
--- src/features/projects/api/projects-api.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "@/lib/api/api-client";
-import { AddProjectType } from "../types/types";
-
-export const addProject = (addProjectData: AddProjectType) => {
-  return apiClient<void>("/rest/v1/projects", {
-    method: "POST",
-    body: JSON.stringify(addProjectData),
-  });
-};
