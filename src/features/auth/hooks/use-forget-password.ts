5ff3f9c1edef59ca143978dcf089e5ee858b80bf
shebll <shebl4900@gmail.com>
2026-07-27 18:25:04 +0300

    refactor: remove tanstack query setup

--- src/features/auth/hooks/use-forget-password.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { forgetPassword } from "../api/auth-api";
-
-export function useForgetPassword() {
-  return useMutation({
-    mutationFn: forgetPassword,
-  });
-}
