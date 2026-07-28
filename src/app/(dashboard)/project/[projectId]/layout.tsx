b58ad2dfad04035e5fed3984af9b24c3ba9860e6
shebll <shebl4900@gmail.com>
2026-07-28 14:51:27 +0300

    feat: add layout for project[id] page and add th Breadcrumb component

---
+++ src/app/(dashboard)/project/[projectId]/layout.tsx
@@ -0,0 +1,23 @@
+import Breadcrumb from "@/components/layout/Breadcrumb";
+import { getProjectById } from "@/features/projects/api/projects-server-api";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{
+    projectId: string;
+  }>;
+};
+
+async function ProjectLayout({ children, params }: Props) {
+  const { projectId } = await params;
+  const projectData = await getProjectById(projectId);
+  return (
+    <div className="flex flex-col gap-2">
+      <Breadcrumb projectName={projectData[0].name.toUpperCase()} />
+      {children}
+    </div>
+  );
+}
+
+export default ProjectLayout;
