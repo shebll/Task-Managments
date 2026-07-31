e1ce9193f326f8416cbd4e57b0bb1d934075604f
shebll <shebl4900@gmail.com>
2026-07-31 16:16:58 +0300

    feat: setup ui updates in popup model and grid

--- src/features/epic/component/EpicsGrid.tsx
+++ src/features/epic/component/EpicsGrid.tsx
@@ -4,19 +4,47 @@ import { useState } from "react";
 
 import EpicCard from "./ui/EpicCard";
 import EpicDetailsModal from "./ui/EpicDetailsModal";
-import { EpicResponse } from "../types/Epic";
+import { EpicResponse, UpdateEpicRequest } from "../types/Epic";
+import { updateEpic } from "../api/epic-api";
+import { useToast } from "@/provider/ToastProvider";
 
 type Props = {
   epics: EpicResponse[];
 };
 
 export default function EpicsGrid({ epics }: Props) {
+  const { showToast } = useToast();
   const [selectedEpic, setSelectedEpic] = useState<EpicResponse | null>(null);
+  const [localEpics, setLocalEpics] = useState<EpicResponse[]>(epics);
 
+  const onEpicUpdate = async (updatedEpic: Partial<UpdateEpicRequest>) => {
+    const prevEpicUi = selectedEpic;
+    // ui updated
+    setSelectedEpic((prev) => (prev ? { ...prev, ...updatedEpic } : prev));
+    setLocalEpics((prev) =>
+      prev.map((epic) =>
+        epic.id == selectedEpic?.id ? { ...epic, ...updatedEpic } : epic,
+      ),
+    );
+
+    try {
+      await updateEpic(selectedEpic!.id, updatedEpic);
+
+      showToast(`Epic Updated `, "success");
+    } catch (error) {
+      // ui rollback
+      setSelectedEpic(prevEpicUi);
+      setLocalEpics((prev) =>
+        prev.map((epic) => (epic.id == selectedEpic?.id ? prevEpicUi! : epic)),
+      );
+
+      showToast("Something went wrong", "error");
+    }
+  };
   return (
     <>
       <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-        {epics.map((epic) => (
+        {localEpics.map((epic) => (
           <EpicCard
             key={epic.id}
             epic={epic}
@@ -28,14 +56,13 @@ export default function EpicsGrid({ epics }: Props) {
       <p className="text-sm text-text-secondary">
         Showing {epics.length} epics
       </p>
-
-      {/* {selectedEpic && (
+      {selectedEpic && (
         <EpicDetailsModal
           epic={selectedEpic}
-          open={!!selectedEpic}
+          onEpicUpdate={onEpicUpdate}
           onClose={() => setSelectedEpic(null)}
         />
-      )} */}
+      )}
     </>
   );
 }
